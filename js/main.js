56cf039d2183958be518b22fe950c1b14f29b4f8
Denzol255 <den-zoloto@mail.ru>
2020-12-15 13:52:39 +0300

    Create: desctop and resposive header and hero

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@ var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
   document
     .querySelector(".navbar-menu")
-    .classList.toggle("navbar-menu--visible");	
+    .classList.toggle("navbar-menu--visible");
+  document
+    .querySelector(".menu-button")
+    .classList.toggle("menu-button--active");	
+    document.querySelector("body").classList.toggle("scroll--lock");
 });
